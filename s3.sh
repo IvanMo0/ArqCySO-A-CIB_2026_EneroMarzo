bef3c17e299f9dab17d1d81f8205104ebbd46e0d
Iván Molina <ivmolinani@uide.edu.ec>
2026-01-21 02:04:26 +0000

    wip: first files

---
+++ s3.sh
@@ -0,0 +1,12 @@
+# 1. Crear un archivo simple con la letra 'a'echo -n "a" > letra.txt
+echo -n "a" > letra.txt
+
+
+# Ver permisos actuales (mira la primera columna, ej: -rw-r--r--)
+ls -l letra.txt
+# Cambiar a permisos 755 (rwx para dueño, r-x para grupo, r-x para otros)
+chmod 755 letra.txt
+# Verificar cambio
+ls -l letra.txt
+
+
